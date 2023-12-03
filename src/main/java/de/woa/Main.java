5f05824b360ee6add11df6b2df2fb717cb7183fe
Jon-Steven Streller <jon-steven.streller@stud.hs-hannover.de>
2023-12-03 17:29:01 +0100

    Heuristic hinzugefügt

--- src/main/java/de/woa/Main.java
+++ src/main/java/de/woa/Main.java
@@ -10,14 +10,20 @@ import de.woa.searchagent.AbstractSearchAgent;
 import de.woa.searchagent.SearchAgentSet;
 
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
 
 
 public class Main {
 
     private static int WHALE_POPULATION = 10;
-    private static int TOTAL_ITERATION = 200;
+    private static int TOTAL_ITERATION = 150;
+
+    private static Fitness fitness;
 
     public static void main(String[] args) throws IOException, DoubleInitializationNotPermittedException, RandomNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         if (!(args.length >= 1)) {
@@ -29,14 +35,56 @@ public class Main {
         if (args.length >= 3) {
             WHALE_POPULATION = Integer.parseInt(args[2]);
         }
-        runWhaleOptimizationAlgorithm(args[0]);
+        if (args.length == 1) {
+            if (args[0].contains("generateHeuristic=")) {
+                String pathToProblem = args[0].split("=")[1];
+                generateIterationHeuristic(pathToProblem);
+            }
+        } else {
+            runWhaleOptimizationAlgorithm(args[0]);
+        }
+
     }
 
-    public static void runWhaleOptimizationAlgorithm(String pathToData) throws IOException, DoubleInitializationNotPermittedException, RandomNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    private static void generateIterationHeuristic(String path) throws DoubleInitializationNotPermittedException, RandomNotFoundException, IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        File directory = new File(path);
+
+        File file = new File("heuristic.csv");
+        FileWriter fr = new FileWriter(file, true);
+        BufferedWriter br = new BufferedWriter(fr);
+        br.write("Started at " + System.currentTimeMillis() / 1000 + "\n");
+        String header = "Total Nodes,Total iterations,Whale population,Improvement,Improvement/Total iterations\n";
+        br.write(header);
+        br.close();
+
+        for (int i = 0; i < Objects.requireNonNull(directory.list()).length; i++) {
+            for (int iterations = 200; iterations <= 1000; iterations += 100) {
+                TOTAL_ITERATION = iterations;
+                for (int agents = 20; agents <= 100; agents += 20) {
+                    WHALE_POPULATION = agents;
+                    runWhaleOptimizationAlgorithm(directory.getPath() + "/" + Objects.requireNonNull(directory.list())[i]);
+
+                    // A separate FileWriter for each iteration
+                    FileWriter dataFileWriter = new FileWriter(file, true);
+                    BufferedWriter bufferedWriter = new BufferedWriter(dataFileWriter);
+
+                    int improvement = (fitness.getBest(0).getFitness() - fitness.getBest(TOTAL_ITERATION - 1).getFitness());
+                    bufferedWriter.write(fitness.getDataset().getSize() + "," + TOTAL_ITERATION + "," + WHALE_POPULATION + ","
+                            + improvement + "," + (improvement / TOTAL_ITERATION - 1));
+                    bufferedWriter.newLine();
+                    bufferedWriter.close();
+                }
+            }
+        }
+
+        fr.close();
+    }
+
+    private static void runWhaleOptimizationAlgorithm(String pathToData) throws IOException, DoubleInitializationNotPermittedException, RandomNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Dataset dataset = Parser.read(pathToData);
 
         // Initialization of the fitness class
-        Fitness fitness = new Fitness(dataset);
+        fitness = new Fitness(dataset);
 
         // Initializes whalePopulation many SearchAgents
         SearchAgentSet<? extends AbstractSearchAgent> searchAgentSet = getAgentSet(dataset, fitness);
