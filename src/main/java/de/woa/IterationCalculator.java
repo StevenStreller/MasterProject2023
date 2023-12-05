312618989a0af2ffc3f211bac02aa33a9954e354
Jon-Steven Streller <jon-steven.streller@stud.hs-hannover.de>
2023-12-05 20:54:09 +0100

    Binary Search eingebaut

---
+++ src/main/java/de/woa/IterationCalculator.java
@@ -0,0 +1,72 @@
+package de.woa;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IterationCalculator {
+
+    public static int[] findClosest(int target) {
+        String csvFilePath = "heuristic_result.csv";
+        char csvDelimiter = ';';
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+            int[] arrayFirstColumn = new int[0];
+            int[] arraySecondColumn = new int[0];
+            int[] arrayThirdColumn = new int[0];
+
+
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(String.valueOf(csvDelimiter));
+                int valueFirstColumn = Integer.parseInt(values[0].trim());
+                int valueSecondColumn = Integer.parseInt(values[1].trim());
+                int valueThirdColumn = Integer.parseInt(values[2].trim());
+
+
+                int[] newArrayFirstColumn = new int[arrayFirstColumn.length + 1];
+                int[] newArraySecondColumn = new int[arraySecondColumn.length + 1];
+                int[] newArrayThirdColumn = new int[arrayThirdColumn.length + 1];
+
+                System.arraycopy(arrayFirstColumn, 0, newArrayFirstColumn, 0, arrayFirstColumn.length);
+                System.arraycopy(arraySecondColumn, 0, newArraySecondColumn, 0, arraySecondColumn.length);
+                System.arraycopy(arrayThirdColumn, 0, newArrayThirdColumn, 0, arrayThirdColumn.length);
+
+                newArrayFirstColumn[arrayFirstColumn.length] = valueFirstColumn;
+                newArraySecondColumn[arraySecondColumn.length] = valueSecondColumn;
+                newArrayThirdColumn[arrayThirdColumn.length] = valueThirdColumn;
+
+                arrayFirstColumn = newArrayFirstColumn;
+                arraySecondColumn = newArraySecondColumn;
+                arrayThirdColumn = newArrayThirdColumn;
+            }
+
+            int low = 0;
+            int high = arrayFirstColumn.length - 1;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if (arrayFirstColumn[mid] == target) {
+                    return new int[]{arrayFirstColumn[mid], arraySecondColumn[mid], arrayThirdColumn[mid]};
+                }
+
+                if (arrayFirstColumn[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+            int diffLow = Math.abs(arrayFirstColumn[low] - target);
+            int diffHigh = Math.abs(arrayFirstColumn[high] - target);
+
+            int closestIndex = (diffLow <= diffHigh) ? low : high;
+
+            return new int[]{arrayFirstColumn[closestIndex], arraySecondColumn[closestIndex], arrayThirdColumn[closestIndex]};
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+}
--- src/main/java/de/woa/Main.java
+++ src/main/java/de/woa/Main.java
@@ -20,27 +20,22 @@ import java.util.Objects;
 
 public class Main {
 
-    private static int WHALE_POPULATION = 10;
-    private static int TOTAL_ITERATION = 150;
-
     private static Fitness fitness;
 
     public static void main(String[] args) throws IOException, DoubleInitializationNotPermittedException, RandomNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         if (!(args.length >= 1)) {
             throw new IllegalArgumentException("Please set the path to the *.tsp");
         }
-        if (args.length >= 2) {
-            TOTAL_ITERATION = Integer.parseInt(args[1]);
-        }
-        if (args.length >= 3) {
-            WHALE_POPULATION = Integer.parseInt(args[2]);
-        }
 
         if (args[0].contains("generateHeuristic=")) {
             String pathToProblem = args[0].split("=")[1];
             generateIterationHeuristic(pathToProblem);
         } else {
-            runWhaleOptimizationAlgorithm(args[0]);
+            Dataset dataset = Parser.read(args[0]);
+            int[] closestIteration = IterationCalculator.findClosest(dataset.getSize());
+            assert closestIteration != null;
+            System.out.println("We have found a similar problem in our knowledge base. We found that for a number of " + closestIteration[0] + " cities, the number of iterations "+ closestIteration[1] +" and the whale population of " + closestIteration[2] + " is the best.");
+            runWhaleOptimizationAlgorithm(dataset, closestIteration[1], closestIteration[2]);
         }
 
 
@@ -59,19 +54,17 @@ public class Main {
 
         for (int i = 0; i < Objects.requireNonNull(directory.list()).length; i++) {
             for (int iterations = 200; iterations <= 1000; iterations += 100) {
-                TOTAL_ITERATION = iterations;
                 for (int agents = 20; agents <= 100; agents += 20) {
                     if (Parser.read(directory.getPath() + "/" + Objects.requireNonNull(directory.list())[i]).getSize() <= 1000) {
-                        WHALE_POPULATION = agents;
-                        runWhaleOptimizationAlgorithm(directory.getPath() + "/" + Objects.requireNonNull(directory.list())[i]);
+                        runWhaleOptimizationAlgorithm(Parser.read(directory.getPath() + "/" + Objects.requireNonNull(directory.list())[i]), iterations, agents);
 
                         // A separate FileWriter for each iteration
                         FileWriter dataFileWriter = new FileWriter(file, true);
                         BufferedWriter bufferedWriter = new BufferedWriter(dataFileWriter);
 
-                        int improvement = (fitness.getBest(0).getFitness() - fitness.getBest(TOTAL_ITERATION - 1).getFitness());
-                        bufferedWriter.write(fitness.getDataset().getSize() + "," + TOTAL_ITERATION + "," + WHALE_POPULATION + ","
-                                + improvement + "," + (improvement / TOTAL_ITERATION - 1));
+                        int improvement = (fitness.getBest(0).getFitness() - fitness.getBest(iterations - 1).getFitness());
+                        bufferedWriter.write(fitness.getDataset().getSize() + "," + agents + "," + agents + ","
+                                + improvement + "," + (improvement / iterations - 1));
                         bufferedWriter.newLine();
                         bufferedWriter.close();
                     }
@@ -83,26 +76,24 @@ public class Main {
         fr.close();
     }
 
-    private static void runWhaleOptimizationAlgorithm(String pathToData) throws IOException, DoubleInitializationNotPermittedException, RandomNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        Dataset dataset = Parser.read(pathToData);
+    private static void runWhaleOptimizationAlgorithm(Dataset dataset, int totalIteration, int whalePopulation) throws DoubleInitializationNotPermittedException, RandomNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 
         // Initialization of the fitness class
         fitness = new Fitness(dataset);
 
         // Initializes whalePopulation many SearchAgents
-        SearchAgentSet<? extends AbstractSearchAgent> searchAgentSet = getAgentSet(dataset, fitness);
+        SearchAgentSet<? extends AbstractSearchAgent> searchAgentSet = getAgentSet(dataset, fitness, whalePopulation);
 
         System.out.println("\n------------------[" + dataset.getType() + "]Whale Optimization Algorithm (WOA)------------------");
-        System.out.println("Total iterations: " + TOTAL_ITERATION + " \\ " + "Whale Population: " + WHALE_POPULATION);
-        System.out.println("Note: You can change the number of iterations and the whale population using the second <int> and third <int> argument.");
+        System.out.println("Total iterations: " + totalIteration + " \\ " + "Whale Population: " + whalePopulation);
 
-        for (int i = 0; i < TOTAL_ITERATION; i++) {
+        for (int i = 0; i < totalIteration; i++) {
             if (i == 0) {
                 searchAgentSet.setLeader(searchAgentSet.getRandom());
             }
             AbstractSearchAgent random = searchAgentSet.getRandom();
             for (AbstractSearchAgent searchAgent : searchAgentSet) {
-                searchAgent.evaluate(i, TOTAL_ITERATION);
+                searchAgent.evaluate(i, totalIteration);
                 searchAgent.updateRoute(random, searchAgentSet.getLeader()); // (3.1)
                 fitness.evaluate(searchAgent, i);
             }
@@ -111,12 +102,12 @@ public class Main {
         fitness.finish();
     }
 
-    private static SearchAgentSet<? extends AbstractSearchAgent> getAgentSet(Dataset dataset, Fitness fitness) throws DoubleInitializationNotPermittedException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    private static SearchAgentSet<? extends AbstractSearchAgent> getAgentSet(Dataset dataset, Fitness fitness, int whalePopulation) throws DoubleInitializationNotPermittedException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         SearchAgentSet<? extends AbstractSearchAgent> searchAgentSet;
         if (dataset.getType().equals("TSP")) {
-            searchAgentSet = new SearchAgentSet<>(de.woa.searchagent.tsp.SearchAgent.class, fitness, WHALE_POPULATION);
+            searchAgentSet = new SearchAgentSet<>(de.woa.searchagent.tsp.SearchAgent.class, fitness, whalePopulation);
         } else if (dataset.getType().equals("SOP")) {
-            searchAgentSet = new SearchAgentSet<>(de.woa.searchagent.sop.SearchAgent.class, fitness, WHALE_POPULATION);
+            searchAgentSet = new SearchAgentSet<>(de.woa.searchagent.sop.SearchAgent.class, fitness, whalePopulation);
         } else {
             throw new IllegalArgumentException("The submitted file is neither *.tsp nor *.sop");
         }
